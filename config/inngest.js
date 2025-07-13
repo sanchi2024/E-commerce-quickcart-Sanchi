a39681c76b739687215f7bc83025c06faa641bfd
Sanchi Deshmukh <deshmukhsanchi2400@gmail.com>
2025-07-13 15:19:21 +0530

    Fix: correct import path for User model

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
-import User from "./models/User";
+import User from "../models/User.js";
+
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
