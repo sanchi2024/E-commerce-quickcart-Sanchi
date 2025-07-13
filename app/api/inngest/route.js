678b6eb60c74a4e1cc75ca15d7d98493985f3da1
Sanchi Deshmukh <deshmukhsanchi2400@gmail.com>
2025-07-13 14:54:14 +0530

    Fix: corrected path and ensured inngest route is set

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,13 +2,11 @@ import { serve } from 'inngest/next';
 
 import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from '../../../config/inngest';
 
-
-export const { GET, POST, PUT} = serve({
-    client: Inngest,
-    functions: [
-        syncUserCreation,
-        syncUserUpdation,
-        syncUserDeletion
-
-    ],
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
+  ],
 });
